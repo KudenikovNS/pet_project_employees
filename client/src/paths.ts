f12b39f69c30ae3906c3cd2ccf912b896e4180ec
Nikita Kudenikov <KudenikovNS@yandex.ru>
2023-05-13 19:29:56 +0400

    First commit: Backend + Login Page

---
+++ client/src/paths.ts
@@ -0,0 +1,9 @@
+export const Paths = {
+  home: '/',
+  emloyeeAdd: '/emloyee/add',
+  emloyeeEdit: '/emloyee/edit',
+  emloyee: '/emloyee',
+  status: '/status',
+  login: '/login',
+  register: '/register'
+} as const;
